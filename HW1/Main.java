59c6d150fc7dce17f28add5af3e58c5bdf8c4bdb
Campbell Lewis <lewisc34@wwu.edu>
2020-02-06 16:05:20 -0800

    Implemented command line arguments

--- HW1/Main.java
+++ HW1/Main.java
@@ -1,9 +1,27 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
+
+    	int rows = 0;
+		int columns = 0;
+
+			try {
+				rows = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				System.out.println("Please enter an integer value for command line argument one and two.");
+			}
+
+
+			try {
+				columns = Integer.parseInt(args[1]);
+			} catch (NumberFormatException e) {
+				System.out.println("Please enter an integer value for command line argument one and two.");
+			}
+
+
     	Player player = new Player();
-    	DungeonMap myMap = new DungeonMap(5,5,player);
+    	DungeonMap myMap = new DungeonMap(rows, columns, player);
     	DungeonGame game = new DungeonGame(player, myMap);
     	game.play();
-    }
+				}
 }
