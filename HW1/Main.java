a9159fd966e71712285fb2e851bfa8d0f48424ef
Campbell Lewis <lewisc34@wwu.edu>
2020-02-06 16:34:49 -0800

    Accounted for command line errors and negative inputs

--- HW1/Main.java
+++ HW1/Main.java
@@ -2,22 +2,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-    	int rows = 0;
-		int columns = 0;
+    	int rows = 10;
+		int columns = 10;
+		boolean defaultDimensions = false;
 
 			try {
 				rows = Integer.parseInt(args[0]);
-			} catch (NumberFormatException e) {
-				System.out.println("Please enter an integer value for command line argument one and two.");
+			} catch (NumberFormatException argumentNotInt) {
+				System.out.println("Command line argument 1 is wrong format. Defaulting to 10 rows.");
+				defaultDimensions = true;
+			} catch (ArrayIndexOutOfBoundsException noArguments) {
+				System.out.println("No command line arguments entered. Defaulting to 10 by 10 map size.");
+				defaultDimensions = true;
 			}
 
 
 			try {
 				columns = Integer.parseInt(args[1]);
 			} catch (NumberFormatException e) {
-				System.out.println("Please enter an integer value for command line argument one and two.");
+				System.out.println("Command line argument 2 is wrong format. Defaulting to 10 columns.");
+				defaultDimensions = true;
+			} catch (ArrayIndexOutOfBoundsException noArguments){
+				defaultDimensions = true;
 			}
 
+			if(defaultDimensions || rows < 1 || columns < 1){
+				rows = 10;
+				columns = 10;
+			}
 
     	Player player = new Player();
     	DungeonMap myMap = new DungeonMap(rows, columns, player);
